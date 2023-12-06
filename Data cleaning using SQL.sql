fa3db6a8db1fd89f2e8a39d65e8fa1c5af8431ca
Mani Setharasipalli <manisetharasipalli@gmail.com>
2023-12-06 11:46:09 +0530

    Update Data cleaning using SQL.sql

--- Data cleaning using SQL.sql
+++ Data cleaning using SQL.sql
@@ -1,97 +1,89 @@
 -- Data cleaning using MySQL
 
 -- The table looks like this before cleaning
-
 SELECT * FROM audible_books;
 
--- We  clean the columns one by one
-SELECT author,narrator FROM audible_books;
+-- We clean the columns one by one
+SELECT author, narrator FROM audible_books;
 
--- In this two cloumns has same problem, we remove Writtenby,Narratedby
- 
--- (SET AUTOCOMMIT=0) this is use when we did mistakes in manipulation process then we  simply use (ROLLBACK)
+-- In these two columns, we have the same problem; we remove 'Written by' and 'Narrated by'
+
+-- (SET AUTOCOMMIT=0) is used when we make mistakes in the manipulation process; then we simply use (ROLLBACK)
+
+-- Update the author column, removing anything before the last colon
+UPDATE audible_books 
+SET author = SUBSTRING_INDEX(author, ':', -1);
+
+-- We have two author names; we'll keep just one
+UPDATE audible_books 
+SET author = SUBSTRING_INDEX(author, ',', 1);
+
+-- Apply the same process to the narrator column
+UPDATE audible_books 
+SET narrator = SUBSTRING_INDEX(narrator, ':', -1);
 
 UPDATE audible_books 
-SET author=SUBSTRING_INDEX(author,':',-1);
+SET narrator = SUBSTRING_INDEX(narrator, ',', 1);
 
--- we have two author names we just take one or leave it  same
--- i am going take just one author name
+-- The 'Name' column does not look good, so we remove '#' and ':'
 UPDATE audible_books 
-SET author=SUBSTRING_INDEX(author,',',1);
-
--- this process will apply to narrator column also
+SET name = SUBSTRING_INDEX(name, '#', 1);
 
 UPDATE audible_books 
-SET narrator=SUBSTRING_INDEX(narrator,':',-1);
+SET name = SUBSTRING_INDEX(name, ':', 1); 
+
+-- We want to change the 'time' column to minutes
+
+-- We add new columns for this manipulation
+ALTER TABLE audible_uncleaned
+ADD COLUMN (hours INT, minutes INT);
+
+-- We assign the values to 'hours' and 'minutes' into two columns and add them to the 'time' column
+
+-- 'Hours' column
+UPDATE audible_books 
+SET hours = SUBSTRING_INDEX(time, ' ', 1);
 
 UPDATE audible_books 
-SET narrator=SUBSTRING_INDEX(author,',',1);
+SET hours = hours * 60;
 
--- Name column is not looks good so we remove '#' and ':' 
-
+-- 'Minutes' column
 UPDATE audible_books 
-SET name=SUBSTRING_INDEX(name,'#',1);
+SET minutes = SUBSTRING_INDEX(SUBSTRING_INDEX(time, ' ', -2), ' ', 1);
 
+-- Add these two columns to the 'time' column 
 UPDATE audible_books 
-SET name=SUBSTRING_INDEX(name,':',1); 
+SET time = hours + minutes;
 
--- we want to change the time column in minutes
-
--- we add new columns for this manipulation
-
-ALTER  TABLE audible_uncleaned
-ADD COLUMN (hours int,minutes int);
-
--- we assign the values to hours and minutes into two columns and add to time column
-
--- hours column
-
-UPDATE audible_books 
-SET hours=SUBSTRING_INDEX(time,' ',1);
-
-UPDATE audible_books 
-SET hours=hours*60;
-
--- minutes column
-UPDATE audible_books 
-SET minutes=SUBSTRING_INDEX(SUBSTRING_INDEX(time,' ',-2),' ',1);
--- add this two columns to time column 
-
-UPDATE audible_books 
-SET time=hours+minutes;
--- Now we drop that columns
-
+-- Now we drop those columns
 ALTER TABLE audible_books
 DROP COLUMN hours,
 DROP COLUMN minutes;
 
--- we rename the column as runtime_in_mins instead of using time
-
+-- We rename the column as 'runtime_in_mins' instead of using 'time'
 ALTER TABLE audible_books
 RENAME COLUMN time TO runtime_in_mins;
 
--- now we change releasedate column into  in this format 'yyyy-mm-dd'
-UPDATE audible_books SET releasedate=DATE_FORMAT(releasedate,'20%d-%m-%y');
-
--- now we take in only stars from the stars column
-
+-- Now we change the 'releasedate' column into this format 'yyyy-mm-dd'
 UPDATE audible_books 
-SET stars=SUBSTRING_INDEX(stars,' out',1);
+SET releasedate = DATE_FORMAT(releasedate, '20%d-%m-%y');
 
--- Now we remove not rated rows or replace it with null
-
+-- Now we take only stars from the 'stars' column
 UPDATE audible_books 
-SET stars=NULLIF(stars,'Not rated yet');
+SET stars = SUBSTRING_INDEX(stars, ' out', 1);
 
--- now we remove commas in the price column
-
+-- Now we remove 'Not rated' rows or replace them with null
 UPDATE audible_books 
-SET Price=REPLACE(price,',','');
+SET stars = NULLIF(stars, 'Not rated yet');
 
+-- Now we remove commas in the 'price' column
+UPDATE audible_books 
+SET Price = REPLACE(price, ',', '');
+
+-- Display the structure of the table
 DESC audible_books;
 
--- now we change the data types also
-
+-- Now we change the data types also
 ALTER TABLE audible_books
 MODIFY name VARCHAR(100),
 MODIFY author VARCHAR(50),
@@ -102,26 +94,12 @@ MODIFY language VARCHAR(30),
 MODIFY stars FLOAT,
 MODIFY price BIGINT;
 
--- we can change column names also
-
+-- We can change column names also
 ALTER TABLE audible_books
 RENAME COLUMN name TO Book_name,
 RENAME COLUMN releasedate TO Release_date;
 
 -- Now the table looks like this after cleaning
-
 SELECT * FROM audible_books;
+
 -- SET AUTOCOMMIT=1
-
-
-
-
-
-
-
-
-
-
-
-
-
